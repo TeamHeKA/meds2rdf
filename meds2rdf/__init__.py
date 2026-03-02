7beea07bd5919d66aa48dbae64a43b7bc1cdb662
Alberto Marfoglia <a.marfoglia@hotmail.it>
2026-03-02 16:17:33 +0000

    fix: include core classes on _init_

--- meds2rdf/__init__.py
+++ meds2rdf/__init__.py
@@ -2,8 +2,10 @@
 
 from importlib.metadata import version as get_version
 
+from .config import Config, MEDSSchema
 from .converter import MedsRDFConverter
+from .sinks import GraphSink, NTriplesSink, TripleSink
 
-__all__ = ["MedsRDFConverter"]
+__all__ = ["MedsRDFConverter", "Config", "MEDSSchema", "GraphSink", "NTriplesSink", "TripleSink"]
 
 __version__ = get_version("meds2rdf")
